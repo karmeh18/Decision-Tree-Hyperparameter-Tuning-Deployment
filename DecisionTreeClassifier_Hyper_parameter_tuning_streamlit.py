9247b3962610206332db428899cf8019f18ac670
Karan Mehta <56735251+karmeh18@users.noreply.github.com>
2023-01-10 18:28:05 +0000

    Update DecisionTreeClassifier_Hyper_parameter_tuning_streamlit.py

--- DecisionTreeClassifier_Hyper_parameter_tuning_streamlit.py
+++ DecisionTreeClassifier_Hyper_parameter_tuning_streamlit.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import streamlit as st
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelEncoder
 scaler=StandardScaler()
 norm=MinMaxScaler()
